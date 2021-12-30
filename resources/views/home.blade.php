57059f19a586ec1f71374d447a41075d150c68aa
STEPHEN WIKEDZI <36662045+swikedzi@users.noreply.github.com>
2021-12-30 14:52:41 +0300

    Dashboard Setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('dashboard')
+
+@section('page_name')
+    Home Page
+@endsection
+
+@section('page_subname')
+    Welcome to Home Page
+@endsection
+
+@section('contents')
+    This is home page...
+@endsection
---
+++ resources/views/layout/rightmenu.blade.php
@@ -0,0 +1,279 @@
+<div role="tabpanel">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active">
+            <a href="#chat" aria-controls="chat" role="tab" data-toggle="tab">Chat</a>
+        </li>
+        <li role="presentation">
+            <a href="#todo" aria-controls="todo" role="tab" data-toggle="tab">Todo</a>
+        </li>
+        <li role="presentation">
+            <a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a>
+        </li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="chat">
+            <div class="search">
+                <div class="form-group is-empty">
+                    <input type="text" class="form-control underline-input" placeholder="Search...">
+                    <span class="material-input"></span>
+                </div>
+            </div>
+            <h6>Recent</h6>
+            <ul>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button" tabindex="0">
+                            <img class="media-object " src="assets/images/xs/avatar1.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <span class="name">Claire Sassu</span>
+                            <span class="message"> If you are going to use a passage of Lorem Ipsum</span>
+                            <span class="badge badge-outline status"></span>
+                        </div>
+                    </div>
+                </li>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button" tabindex="0">
+                            <img class="media-object " src="assets/images/xs/avatar8.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="name">Maggie jackson</span>
+                                <span class="message">All the Lorem Ipsum generators on the Internet</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button" tabindex="0">
+                            <img class="media-object " src="assets/images/xs/avatar3.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="name">Joel King</span>
+                                <span class="message">The standard chunk of Lorem Ipsum used</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button" tabindex="0">
+                            <img class="media-object " src="assets/images/xs/avatar6.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="name">Isabella</span>
+                                <span class="message">There are many variations of passages of Lorem</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button" tabindex="0">
+                            <img class="media-object " src="assets/images/xs/avatar5.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="name">Alexander</span>
+                                <span class="message">Contrary to popular belief, Lorem Ipsum</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <h6>Contacts</h6>
+            <ul>
+                <li class="offline">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button">
+                            <img class="media-object " src="assets/images/xs/avatar5.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="mt-5 name">Matthew</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="offline">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button">
+                            <img class="media-object " src="assets/images/xs/avatar4.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="mt-5 name">Aaliyah</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="offline">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button">
+                            <img class="media-object " src="assets/images/xs/avatar8.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="mt-5 name">Arianna</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="online">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button">
+                            <img class="media-object " src="assets/images/xs/avatar9.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="mt-5 name">Scarlett</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="offline">
+                    <div class="media">
+                        <a class="pull-left thumb thumb-sm" role="button">
+                            <img class="media-object " src="assets/images/xs/avatar10.jpg" alt=""> </a>
+                        <div class="media-body">
+                            <div class="media-body">
+                                <span class="mt-5 name">David</span>
+                                <span class="badge badge-outline status"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="todo">
+            <div class="form-group">
+                <input type="text" value="" placeholder="Create new task..." class="form-control" />
+                <span class="fa fa-plus"></span>
+            </div>
+            <h6>Today</h6>
+            <ul class="todo-list">
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> Initialize the project</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> Create the main structure</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> Create the main structure</label>
+                    </div>
+                </li>
+            </ul>
+            <h6>Tomorrow</h6>
+            <ul class="todo-list">
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> Initialize the project</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> Create the main structure</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="optionsCheckboxes"> displayed in a normal space!</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="settings">
+            <h6>Chat Settings</h6>
+            <ul class="settings">
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">Show Offline Users</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox" checked="">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">Show Fullname</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">History Enable</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox" checked="">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">Show Locations</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox" checked="">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">Notifications</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="form-group">
+                        <label class="col-xs-8 control-label">Show Undread Count</label>
+                        <div class="text-right col-xs-4 control-label">
+                            <div class="togglebutton">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
